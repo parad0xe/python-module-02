091ec34285d076e909e58fe4c8547057875581bc
Nathan Lallemand <nlallema@z2r11p5.42lyon.fr>
2026-01-05 19:28:35 +0100

    add functions to tests all errors

--- ex2/ft_custom_errors.py
+++ ex2/ft_custom_errors.py
@@ -20,16 +20,53 @@ class WaterError(GardenError):
     pass
 
 
-if __name__ == "__main__":
+def test_garden_error() -> None:
+    """
+    Test an GardenError
+    """
     try:
         raise GardenError("An error occurred in the garden")
     except GardenError as e:
         print(f"Caught GardenError: {e}")
+
+
+def test_plant_error() -> None:
+    """
+    Test an PlantError
+    """
+    try:
+        raise PlantError("The plant is dead")
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+
+
+def test_water_error() -> None:
+    """
+    Test an WaterError
+    """
+    try:
+        raise WaterError("Watering with acid !")
+    except WaterError as e:
+        print(f"Caught WaterError: {e}")
+
+
+def test_all_garden_errors() -> None:
+    """
+    Test an PlantError and WaterError catched by GardenError
+    """
     try:
         raise PlantError("The plant is dead")
     except GardenError as e:
-        print(f"Caught PlantError: {e}")
+        print(f"Caught GardenError: {e}")
+
     try:
         raise WaterError("Watering with acid !")
     except GardenError as e:
-        print(f"Caught WaterError: {e}")
+        print(f"Caught GardenError: {e}")
+
+
+if __name__ == "__main__":
+    test_garden_error()
+    test_plant_error()
+    test_water_error()
+    test_all_garden_errors()
