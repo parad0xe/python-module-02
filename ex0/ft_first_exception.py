d4d5f3e8c609060bb74e7104fa62c7d8aee35ad1
Nathan Lallemand <nlallema@z2r11p5.42lyon.fr>
2026-01-05 15:33:49 +0100

    updpate ex0: add constants, improve error management

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,23 +1,45 @@
 
+MIN_TEMPERATURE = 0
+MAX_TEMPERATURE = 40
+
+
 def check_temperature(value: str) -> bool:
     try:
+        print(f"\nTesting temperature: {value}")
         temperature = int(value)
 
-        if temperature < 0:
-            print("[ERROR] The temperature must be not lower than 0")
+        if temperature < MIN_TEMPERATURE:
+            print(
+                f"Error: {value}°C is too cold for plants",
+                f"(min {MIN_TEMPERATURE}°C)"
+            )
             return False
-        if temperature > 40:
-            print("[ERROR] The temperature must be not greater than 40")
+        if temperature > MAX_TEMPERATURE:
+            print(
+                f"Error: {value}°C is too hot for plants",
+                f"(min {MAX_TEMPERATURE}°C)"
+            )
             return False
+        print(f"Temperature {value}°C is perfect for plants!")
         return True
     except ValueError:
-        print("[ERROR] The temperature must be a valid integer")
+        print(f"Error: '{value}' is not a valid number")
+        return False
+    except TypeError:
+        print(f"Error: '{value}' is not a valid type")
+        return False
+    except Exception:
+        print(f"Error: '{value}' could not be converted to int")
         return False
 
 
 if __name__ == "__main__":
-    print("0:", check_temperature(0))
-    print("35:", check_temperature(35))
-    print("40:", check_temperature(40))
-    print("-12:", check_temperature(-12))
-    print("70:", check_temperature(70))
+    print("=== Garden Temperature Checker ===")
+
+    check_temperature(0)
+    check_temperature(35)
+    check_temperature(40)
+    check_temperature(-12)
+    check_temperature(70)
+    check_temperature("abc")
+    check_temperature(("100", "-50"))
